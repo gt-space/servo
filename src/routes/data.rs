0bf8acde8a63c76717f97e097dbdafa845d3aa67
AndrewCarlisleECE <andrewcarlisleece@gmail.com>
2024-03-04 15:13:56 -0500

    Added basic unit test for make_hdf5_file as well as fixing valve_states being saved as an i32 instead of an i8

--- src/routes/data.rs
+++ src/routes/data.rs
@@ -29,7 +29,7 @@ static EXPORT_FILE_INDEX_ATOMIC : AtomicU32 = AtomicU32::new(0);
 
 #[cfg_attr(not(debug_assertions), no_panic::no_panic)]
 #[inline(never)]
-/// A function that creates an HDF5 file at a given path containing the timestamps, sensor, and valve values in each vehicle state as specified in sensor_names and valve_names
+/// A function that creates an HDF5 file at a given path containing the timestamps, sensor, and valve values as specified in sensor_names and valve_names in each vehicle state
 pub fn make_hdf5_file(sensor_names : &[std::string::String], valve_names : &[std::string::String], vehicle_states : &[(f64, VehicleState)], path : &Path) -> hdf5::Result<()>{
 	// Create the HDF5 file
 	let file : hdf5::File = hdf5::File::create(path)?;
@@ -101,15 +101,15 @@ pub fn make_hdf5_file(sensor_names : &[std::string::String], valve_names : &[std
 	// Will make all values of valves metadata later
 	for name in valve_names {
 		// A vector of all the values of the valve in each timeframe
-		let mut state_vec : Vec<i32> = Vec::<i32>::with_capacity(vehicle_states.len());
+		let mut state_vec : Vec<i8> = Vec::<i8>::with_capacity(vehicle_states.len());
 		
 		// Yes I know iterating through the vehicle states for every sensor / valve is dumb,
 		// but I'm avoiding storing the entirety of the vehicle state in memory twice, so each
 		// sensor is grabbed seperately
 		for (_, state) in vehicle_states {
-			let measurement = state.valve_states.get(name);
+			let valve_state = state.valve_states.get(name);
 			// Put in bad data if nothing is found
-			match measurement {
+			match valve_state {
 				Some(x) => {
 					if !seen_valve_states.contains(x) { // Keep track of seen valve states
 						seen_valve_states.insert(x.clone());
@@ -313,5 +313,138 @@ pub async fn export(
 			)
 		},
 		_ => Err(error::ErrorBadRequest("invalid export format")),
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use rand::Rng;
+	use rand::prelude::*;
+	use std::collections::HashMap;
+	use std::time::Duration;
+	use super::*;
+    #[test]
+	fn test_hdf5_file_creation() {
+		// Do the same test a few times just cause this does use RNG
+		for _ in 0..8 {
+			let path : &Path = Path::new("./CompilingTestsExportSample.hdf5");
+			
+			let count = 64;
+
+			let mut vehicle_states = Vec::<(f64, VehicleState)>::with_capacity(count);
+			
+			let mut rng = rand::thread_rng();
+			let mut time : f64 = 0.0;
+
+			let mut timestamps_vec : Vec<f64> = Vec::<f64>::with_capacity(count);
+			
+			let sensor_units  : [Unit; 4] = [Unit::Amps, Unit::Psi, Unit::Volts, Unit::Kelvin];
+
+			let valve_names : [std::string::String; 4] = [std::string::String::from("V1"), std::string::String::from("V2"), std::string::String::from("V3"),std::string::String::from("V4")];
+			let mut valve_state_vecs : [Vec<i8>; 4] = [Vec::<i8>::with_capacity(count), Vec::<i8>::with_capacity(count), Vec::<i8>::with_capacity(count), Vec::<i8>::with_capacity(count)];
+
+			let mut seen_valve_states : Vec<ValveState> = Vec::<ValveState>::with_capacity(10);
+
+			let sensor_names : [std::string::String; 4] = [std::string::String::from("S1"), std::string::String::from("S2"), std::string::String::from("S3"),std::string::String::from("S4")];
+			let mut sensor_state_vecs : [Vec<f64>; 4] = [Vec::<f64>::with_capacity(count), Vec::<f64>::with_capacity(count), Vec::<f64>::with_capacity(count), Vec::<f64>::with_capacity(count)];
+			let mut sensor_unit_vecs  : [Vec<i8>; 4] = [Vec::<i8>::with_capacity(count), Vec::<i8>::with_capacity(count), Vec::<i8>::with_capacity(count), Vec::<i8>::with_capacity(count)];
+			
+			for _ in 0..count {
+				let mut state = VehicleState { valve_states : HashMap::<String, ValveState>::new(), sensor_readings : HashMap::<String, Measurement>::new(), update_times : HashMap::<String, f64>::new() };
+				
+				for i in 0..4 {
+					if rng.next_u32() % 10 > 0 { // have some "empty" timeframes for a bit of data 
+						let valve_state_temp = match rng.next_u32() % 5 {
+							0 => ValveState::Disconnected,
+							1 => ValveState::Open,
+							2 => ValveState::Closed,
+							3 => ValveState::CommandedOpen,
+							4 => ValveState::CommandedClosed,
+							_ => ValveState::Disconnected,
+						};
+						if !seen_valve_states.contains(&valve_state_temp) {
+							seen_valve_states.push(valve_state_temp.clone());
+						}
+						state.valve_states.insert(valve_names[i].clone(), valve_state_temp.clone());
+						valve_state_vecs[i].push(valve_state_temp as i8);
+
+					} else {
+						valve_state_vecs[i].push(-69);
+					}
+				}
+				
+				for i in 0..4 {
+					if rng.next_u32() % 10 > 0 { // have some "empty" timeframes for a bit of data 
+						let x : f64 = rng.gen::<f64>() * 5.0;
+						sensor_state_vecs[i].push(x);
+						sensor_unit_vecs[i].push(sensor_units[i] as i8);
+						state.sensor_readings.insert(sensor_names[i].clone(), Measurement { value : x, unit : sensor_units[i] });
+					} else {
+						sensor_state_vecs[i].push(-6942069420.0);
+						sensor_unit_vecs[i].push(-69);
+					}
+				}
+				vehicle_states.push((time, state));
+				timestamps_vec.push(time);
+				time += 0.1;
+			}
+			let make_result = make_hdf5_file(&sensor_names, &valve_names, &vehicle_states, path).expect("HDF5 should not error out when making this basic dataset");
+
+			let file = hdf5::File::open(path).expect("File should exist after make_hdf5_file runs, as make_hdf5_file literally makes"); // 
+
+			// You have to close groups to be able to close a file, so we simply do all of the HDF5 operations inside of a namespace like this so they automatically deconstruct and close.
+			{
+				// get metadata group / ensure it exists
+				let metadata_group = file.group("metadata").expect("HDF5 file for data exports should always have metadata group in it");
+
+				// ensure timestamps are accurate
+				{
+					let timestamps = metadata_group.dataset("timestamps").expect("HDF5 file for data exports should always have timestamps in the metadata group");
+					assert_eq!(timestamps.shape(), vec![count]);
+					assert_eq!(timestamps.read_raw::<f64>().expect("timestamps should be readable."), timestamps_vec);
+				}
+
+				// ensure valve_state_id lookup attributes are accurate
+				{
+					let valve_state_ids = metadata_group.group("valve_state_ids").expect("HDF5 file for data exports should always have valve_state_ids group in the metadata group");
+					assert_eq!(valve_state_ids.attr_names().expect("valve_state_ids should have attributes.").len(), seen_valve_states.len()); // make sure they have equal element counts
+					for state in seen_valve_states {
+						let attr_value = valve_state_ids.attr(&state.to_string())
+							.expect("valve_state_ids should have all valve states that are seen during creation of a dataset in it's attributes")
+							.read_raw::<i8>()
+							.expect("valve_state_ids attributes should be readable as a signed byte");
+						assert_eq!(attr_value.len(), 1); // This should be a single value
+						assert_eq!(attr_value[0], state as i8);
+							
+					}
+				}
+
+				// ensure valve readings are accurate
+				let valves_group = file.group("valves").expect("HDF5 file for data exports should always have valve folder in it");
+				for i in 0..4 {
+					let name = &valve_names[i];
+					let valve_ds = valves_group.dataset(&name).expect("All valves specified should have a dataset");
+					assert_eq!(valve_ds.shape(), vec![count]);
+					assert_eq!(valve_ds.read_raw::<i8>().expect("valve state dataset should be readable."), valve_state_vecs[i]);
+				}
+				
+				// ensure sensor readings are accurate
+				let sensors_group = file.group("sensors").expect("HDF5 file for data exports should always have sensor folder in it");
+				for i in 0..4 {
+					let name = &sensor_names[i];
+					let this_sensor_group = sensors_group.group(&name).expect("All sensors specified should have a group");
+					let sensor_ds = this_sensor_group.dataset("readings").expect("All sensor groups should have a readings dataset");
+					let unit_ds = this_sensor_group.dataset("units").expect("All sensor groups should have a unit dataset");
+					assert_eq!(sensor_ds.shape(), vec![count]);
+					assert_eq!(unit_ds.shape(), vec![count]);
+					assert_eq!(sensor_ds.read_raw::<f64>().expect("sensor value dataset should be readable."), sensor_state_vecs[i]);
+					assert_eq!(unit_ds.read_raw::<i8>().expect("sensor unit dataset should be readable."), sensor_unit_vecs[i]);
+				}
+			}
+
+			let _ = file.close().expect("File should properly close after reading hdf5 values from it (How did this even happen?)");
+
+			let _ = std::fs::remove_file(path).expect("You should be able to delete the HDF5 file after closing it ");
+		}
 	}
 }
